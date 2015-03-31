92b13ab0fdcab741df8a09be55fdba9b3e01aaad
Thomas Köppe <tkoeppe@google.com>
2015-03-31 21:18:11 +0100

    Improved documentation

--- matrix_format.hpp
+++ matrix_format.hpp
@@ -1,3 +1,15 @@
+// Matrix pretty-printing tools.
+//
+// 1. Inline printer:
+//
+//    matrix<int> m = make_a_matrix();
+//    std::cout << "The matrix is " << print_inline(m) << ".\n";
+//
+// 2. Full multi-line pretty-print with fixed field width:
+//
+//    matrix<int> m = make_a_matrix();
+//    std::cout << "The matrix is:\n" << print(m, 4);
+
 #ifndef H_MATRIX_FORMAT
 #define H_MATRIX_FORMAT
 
@@ -9,9 +21,9 @@
 template <typename T>
 struct matrix_printer
 {
-    matrix<T> const & mat;
-    char const * pre;
-    int wd;
+    matrix<T> const & mat;     // the matrix
+    char const * pre;          // per-line prefix (e.g. indentation)
+    int wd;                    // field width
 
     matrix_printer(matrix<T> const & m, char const * p, int w = 0)
     : mat(m), pre(p), wd(w) {}
@@ -20,20 +32,24 @@ struct matrix_printer
 template <typename T>
 struct inline_matrix_printer
 {
-    matrix<T> const & mat;
-    char const * outer_sep;
-    char const * inner_sep;
+    matrix<T> const & mat;     // the matrix
+    char const * outer_sep;    // separator between rows
+    char const * inner_sep;    // separator between columns
 
     inline_matrix_printer(matrix<T> const & m, char const * outer, char const * inner)
     : mat(m), outer_sep(outer), inner_sep(inner) {}
 };
 
+// Returns a multi-line matrix pretty printer with the given field width
+// and per-line prefix.
 template <typename T>
-matrix_printer<T> print(matrix<T> const & m, char const * p, std::size_t w)
+matrix_printer<T> print(matrix<T> const & m, int w, char const * p = "")
 {
     return matrix_printer<T>(m, p, w);
 }
 
+// Returns an inline matrix pretty printer with the given row and column
+// separators.
 template <typename T>
 inline_matrix_printer<T> print_inline(matrix<T> const & m, char const * outer = ", ", char const * inner = ", ")
 {
